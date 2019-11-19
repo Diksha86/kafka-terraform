68f15ceb38d1264aa24756dda1334d87eabb7a5b
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-11-19 07:50:47 +0000

    Update kafka.tf

--- kafka.tf
+++ kafka.tf
@@ -10,12 +10,12 @@ resource "google_compute_address" "kafkaip" {
 }
 
 
-resource "google_compute_instance" "kafka" {
+resource "google_compute_instance" "kafka0" {
   name         = "${var.instance_name}"
   machine_type = "n1-standard-2"
   zone         = "us-east1-b"
 
-  tags = ["name", "kafka", "http-server"]
+  tags = ["name", "kafka0", "http-server"]
 
   boot_disk {
     initialize_params {
@@ -36,7 +36,69 @@ resource "google_compute_instance" "kafka" {
     }
   }
   metadata = {
-    name = "kafka"
+    name = "kafka0"
+  }
+
+  metadata_startup_script = "sudo yum update -y;sudo yum install git -y; sudo git clone https://github.com/Diksha86/kafka-terraform.git; cd kafka-terraform ; sudo chmod 777 *; sudo sh kafka.sh"
+}
+resource "google_compute_instance" "kafka1" {
+  name         = "${var.instance_name}"
+  machine_type = "n1-standard-2"
+  zone         = "us-east1-b"
+
+  tags = ["name", "kafka1", "http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-7-v20180129"
+    }
+  }
+
+  // Local SSD disk
+  #scratch_disk {
+  #}
+
+  network_interface {
+    network    = "${var.kafkavpc}"
+    subnetwork = "${var.kafkasub}"
+    access_config {
+      // Ephemeral IP
+      nat_ip = "${google_compute_address.kafkaip.address}"
+    }
+  }
+  metadata = {
+    name = "kafka1"
+  }
+
+  metadata_startup_script = "sudo yum update -y;sudo yum install git -y; sudo git clone https://github.com/Diksha86/kafka-terraform.git; cd kafka-terraform ; sudo chmod 777 *; sudo sh kafka.sh"
+}
+resource "google_compute_instance" "zookeeper" {
+  name         = "${var.instance_name}"
+  machine_type = "n1-standard-2"
+  zone         = "us-east1-b"
+
+  tags = ["name", "zookeeper", "http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-7-v20180129"
+    }
+  }
+
+  // Local SSD disk
+  #scratch_disk {
+  #}
+
+  network_interface {
+    network    = "${var.kafkavpc}"
+    subnetwork = "${var.kafkasub}"
+    access_config {
+      // Ephemeral IP
+      nat_ip = "${google_compute_address.kafkaip.address}"
+    }
+  }
+  metadata = {
+    name = "zookeeper"
   }
 
   metadata_startup_script = "sudo yum update -y;sudo yum install git -y; sudo git clone https://github.com/Diksha86/kafka-terraform.git; cd kafka-terraform ; sudo chmod 777 *; sudo sh kafka.sh"
