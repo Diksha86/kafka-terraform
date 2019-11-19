01ccbcd2d3d6a5fce69c9e14db5d969135660e0a
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-11-19 08:21:46 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,12 @@
 variable "region" {}
 variable "gcp_project" {}
 variable "credentials" {}
-variable "kafkavpc" {}
-variable "kafkasub" {}
+variable "kafka1vpc" {}
+variable "kafka1sub" {}
+variable "kafka2vpc" {}
+variable "kafka2sub" {}
+variable "kafka3vpc" {}
+variable "kafka3sub" {}
 variable "instance_name1" {}
 variable "instance_name2" {}
 variable "instance_name3" {}
