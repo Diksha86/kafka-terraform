5782525c24d23bb8b1adfecf9fd33af0290e0518
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-11-19 06:21:03 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {}
+variable "gcp_project" {}
+variable "credentials" {}
+variable "kafkavpc" {}
+variable "kafkasub" {}
+variable "instance_name" {}
+variable "kafka_instance_ip_name" {}
+variable "kafka_instance_ip_region" {}
