59a814ae67128aa08ba29d020b21ff63a9f37c8c
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-11-19 09:33:19 +0000

    Create kafka1.sh

---
+++ kafka1.sh
@@ -0,0 +1,7 @@
+sudo yum -y install java-1.8.0-openjdk
+sudo yum -y install wget
+wget http://mirrors.estointernet.in/apache/kafka/2.3.1/kafka_2.11-2.3.1.tgz
+tar -xzf kafka_2.11-2.3.1.tgz
+export PATH="$PATH:$HOME/bin:$HOME/kafka_2.11-2.3.1/bin"
+source ~/.bash_profile
+/root/kafka-terraform/kafka_2.11-2.3.1/bin/kafka-server-start.sh /root/kafka-terraform/kafka_2.11-2.3.1/config/server.properties
