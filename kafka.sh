551871a058e83d64640ec50d3eb679e678abc951
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-11-20 11:33:01 +0000

    Update kafka.sh

--- kafka.sh
+++ kafka.sh
@@ -3,5 +3,4 @@ sudo yum -y install wget
 wget http://mirrors.estointernet.in/apache/kafka/2.3.1/kafka_2.11-2.3.1.tgz
 tar -xzf kafka_2.11-2.3.1.tgz
 # export PATH="$PATH:$HOME/bin:$HOME/kafka-terraform/kafka_2.11-2.3.1/bin"
-# source ~/.bash_profile
 /kafka-terraform/kafka_2.11-2.3.1/bin/zookeeper-server-start.sh /kafka-terraform/kafka_2.11-2.3.1/config/zookeeper.properties > /dev/null 2>&1 &
