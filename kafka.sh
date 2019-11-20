1fd45ade880913f6edd5ac7ba6dab1eb67434f8f
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-11-20 06:19:01 +0000

    Update kafka.sh

--- kafka.sh
+++ kafka.sh
@@ -2,5 +2,5 @@ sudo yum -y install java-1.8.0-openjdk
 sudo yum -y install wget
 wget http://mirrors.estointernet.in/apache/kafka/2.3.1/kafka_2.11-2.3.1.tgz
 tar -xzf kafka_2.11-2.3.1.tgz
-export PATH="$PATH:$HOME/bin:$HOME/kafka_2.11-2.3.1/bin"
+export PATH="$PATH:$HOME/bin:$HOME/kafka-terraform/kafka_2.11-2.3.1/bin"
 source ~/.bash_profile
